215b0bd38b6267233918e81d1012cbf681ff8594
kirrog <kirrog@zertan.ru>
2023-04-29 20:09:21 +0300

    feat: example

--- nn_scripts/model_test.py
+++ nn_scripts/model_test.py
@@ -1,8 +1,9 @@
 import torch
 
 from nn_scripts.models.resnet import ResNet, ResidualBlock
-
-images = torch.zeros((1,3, 150, 300))
+MODEL_PATH_IN = "../model/unique_feature/base/ep_010_acc_0.953290.bin"
+images = torch.zeros((1, 3, 150, 300))
 model = ResNet(ResidualBlock, [3, 4, 6, 3])
+model.load_state_dict(torch.load(MODEL_PATH_IN))
 outputs = model(images)
 print(outputs.size())
