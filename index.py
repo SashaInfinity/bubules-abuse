093cf6eea3cd6b357de73a641149805a500da5d3
Alexandr Palyuk <wowmegodc@yandex.ru>
2023-04-30 12:33:08 +0200

    did something but i dont remember

--- index.py
+++ index.py
@@ -13,11 +13,14 @@ from nn_scripts.model_test import predict_space_state
 from selenium.webdriver.common.by import By
 
 bbox = {"top": 0, "left": 0, "width": 1280, "height": 728}
+state = 0
 
 def press_space(driver):
-    ActionChains(driver).send_keys(Keys.SPACE).key_down(Keys.SPACE).perform()
+    driver.find_element(By.XPATH,'//*[@id="ir__canvas"]')
+    ActionChains(driver).key_down(Keys.SPACE).perform()
 
 def up_space(driver):
+    driver.find_element(By.XPATH,'//*[@id="ir__canvas"]')
     ActionChains(driver).key_up(Keys.SPACE).perform()
 
 def connect_to_driver(address):
@@ -27,47 +30,52 @@ def connect_to_driver(address):
     return driver
 
 def analyze_screenshot(driver,screenshot):
+    global state
     state = predict_space_state(screenshot)
-    print("predicted : " + str(time.time()))
-
     if (state == 1):
         press_space(driver)
     else:
         up_space(driver)
 
 
-def make_screenshot(sct):
-        img = numpy.asarray(sct.grab(bbox))
+def make_screenshot(sct):    
+    img = numpy.asarray(sct.grab(bbox))
 
-        if img.shape[2] == 4:
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    if img.shape[2] == 4:
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
 
-        reduction_percentage = 0.6
-        resized_img = cv2.resize(img, None, fx=reduction_percentage, fy=reduction_percentage, interpolation=cv2.INTER_AREA)
-        
-        x, y, w, h = 0, 69, 768, 436
-        cropped_img = resized_img[y:y+h, x:x+w]
+    reduction_percentage = 0.6
+    resized_img = cv2.resize(img, None, fx=reduction_percentage, fy=reduction_percentage, interpolation=cv2.INTER_AREA)
+    
+    x, y, w, h = 0, 69, 768, 436
+    cropped_img = resized_img[y:y+h, x:x+w]
 
-        resized = cv2.resize(cropped_img, (300, 150), interpolation=cv2.INTER_AREA)
+    resized = cv2.resize(cropped_img, (300, 150), interpolation=cv2.INTER_AREA)
 
-        return resized
+    return resized
 
 
 def screen_record_start() -> int:
+    global state
+
     sct = mss.mss()
 
-    driver_address = "127.0.0.1:51796"
+    driver_address = "127.0.0.1:56354"
     driver = connect_to_driver(driver_address)
     print("READY")
 
     while True:
-        print("start: " + str(time.time()))
+        # print("start: " + str(time.time()))
 
         screenshot = make_screenshot(sct)
-        print("screenshot : " + str(time.time()))
+        # if (state == 1):
+        #     up_space(driver)
+        # else:
+        #     press_space(driver)
+        # print("screenshot : " + str(time.time()))
 
         analyze_screenshot(driver, screenshot)
-        print("action-done : " + str(time.time()))
+        # print("action-done : " + str(time.time()))
 
 
         # title = "[MSS] FPS benchmark"
--- nn_scripts/model_test.py
+++ nn_scripts/model_test.py
@@ -1,30 +1,30 @@
 import torch
-import cv2
+# import cv2
 import time
 
-from models.resnet import ResNet, ResidualBlock
+from nn_scripts.models.resnet import ResNet, ResidualBlock
 MODEL_PATH_IN = "./models/ep_001_acc_0.951565.bin"
-IMG_PATH = "./123.jpg"
-img = cv2.imread(str(IMG_PATH))
-reduction_percentage = 0.6
-resized_img = cv2.resize(img, None, fx=reduction_percentage, fy=reduction_percentage, interpolation=cv2.INTER_AREA)
+# IMG_PATH = "./123.jpg"
+# img = cv2.imread(str(IMG_PATH))
+# reduction_percentage = 0.6
+# resized_img = cv2.resize(img, None, fx=reduction_percentage, fy=reduction_percentage, interpolation=cv2.INTER_AREA)
 
-x, y, w, h = 0, 69, 768, 436
-cropped_img = resized_img[y:y+h, x:x+w]
+# x, y, w, h = 0, 69, 768, 436
+# cropped_img = resized_img[y:y+h, x:x+w]
 
-resized = cv2.resize(cropped_img, (300, 150), interpolation=cv2.INTER_AREA)
+# resized = cv2.resize(cropped_img, (300, 150), interpolation=cv2.INTER_AREA)
 
 def predict_space_state(screenshot):
-    print("start: " + str(time.time()))
+    # print("start: " + str(time.time()))
     tensor = torch.from_numpy(screenshot).permute(2, 0, 1).unsqueeze(0).float()
 
     model = ResNet(ResidualBlock, [3, 2, 3, 1])
-    model.load_state_dict(torch.load(MODEL_PATH_IN))
+    model.load_state_dict(torch.load(MODEL_PATH_IN, map_location=torch.device('cpu')))
     outputs = model(tensor)
 
     space_up_probability = outputs[0][0].item()
     space_down_probability = outputs[0][1].item()
-    print("finish: " + str(time.time()))
+    # print("finish: " + str(time.time()))
 
 
     if space_up_probability > space_down_probability:
@@ -32,4 +32,4 @@ def predict_space_state(screenshot):
     else:
         return 1
 
-predict_space_state(resized)
+# predict_space_state(resized)
